509d8bd880e35ca128b9e0700f1d92429f4cb315
Diego Vilhalva <diegorocha450@yahoo.com>
2025-05-09 09:03:10 -0300

    upadte services.php

--- config/services.php
+++ config/services.php
@@ -15,12 +15,12 @@ return [
     */
 
     'postmark' => [
-        'token' => env('POSTMARK_TOKEN'),
+        'token' => env('POSTMARK_TOKEN',''),
     ],
 
     'ses' => [
-        'key' => env('AWS_ACCESS_KEY_ID'),
-        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'key' => env('AWS_ACCESS_KEY_ID',''),
+        'secret' => env('AWS_SECRET_ACCESS_KEY',''),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
@@ -30,8 +30,8 @@ return [
 
     'slack' => [
         'notifications' => [
-            'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
-            'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
+            'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN',''),
+            'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL',''),
         ],
     ],
 
